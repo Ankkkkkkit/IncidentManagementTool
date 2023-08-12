d53e0dc71c438c0229af627fb72b6a4c52d6eb50
Ankkkkkkit <ankitsingh02003@gmail.com>
2023-08-12 20:21:27 +0530

    Initialized Spring Project and added ER diagram

---
+++ src/test/java/com/sail/incidentTool/IncidentToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sail.incidentTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
